4b4e9bbc90c6eb2f92ccd723e5f9f7c78cf67802
Joyce Chun <wooyangee@gmail.com>
2018-04-28 18:43:13 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,6 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 let index = 0;
 
 function init(e) {
+  document.addEventListener('keydown', function(e) {
 const key = parseInt(e.detail || e.which || e.location);
 
 if (key === code[index]) {

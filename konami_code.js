34a0c8f1853ecb2fd7c33b27fb80d67d00d927ae
Joyce Chun <wooyangee@gmail.com>
2018-04-28 18:37:24 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init(e) {
-const key = parseInt(e.detail || e.which);
+const key = parseInt(e.detail || e.which || e.location);
 
-if (key === alphabet[index])
+if (key === code[index]) {
+index++;
+
+if (index === code.length) {
+  alert()
+}
 }
